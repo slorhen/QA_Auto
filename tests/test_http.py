0c206164f2912fcf520c9f7cd67c603e67ae7bec
Larysa Semenysta <slorhen@gmail.com>
2023-01-29 18:59:41 +0200

    Module 16

--- tests/test_http.py
+++ tests/test_http.py
@@ -21,6 +21,6 @@ def test_second_request():
 
 @pytest.mark.http
 def test_status_code_request():
-    r = requests.get ('https://api.github.com/users/sergii_')
+    r = requests.get ('https://api.github.com/users/sergii_butenko')
 
     assert r.status_code == 404
